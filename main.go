f2fdf8774e31f594317425da5360103e40017c4f
Cristhyano <crisdpa@tuta.io>
2022-05-04 11:54:26 -0300

    separate routes from main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -16,40 +15,12 @@ type blog struct {
 	tag   string
 }
 
-func (bl *blog) httpFunc(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/", "/index.html":
-		executeTemplate(w, "index.html", blogposts.FrontPage(bl.posts))
-		return
-	case "/blog":
-		bl.tag = r.URL.Query().Get("tag")
-		executeTemplate(w, "blog.html", blogposts.BlogArchive(bl.posts, bl.tag))
-		return
-	default:
-		http.NotFound(w, r)
-		return
-	}
-}
-
-func executeTemplate(w http.ResponseWriter, templ string, content interface{}) {
-	templates := template.Must(template.ParseGlob("./ui/html/*.html"))
-	err := templates.ExecuteTemplate(w, templ, content)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func makePostHandler(post blogposts.Post) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		executeTemplate(w, "post.html", post)
-	}
-}
-
 func main() {
 	var (
 		posts []blogposts.Post
 		tag   string
 	)
+
 	deploy := flag.Bool("deploy", false, "get environment $PORT")
 	flag.Parse()
 
@@ -65,17 +36,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", bl.httpFunc)
-	mux.HandleFunc("/blog", bl.httpFunc)
-
-	for _, post := range bl.posts {
-		mux.HandleFunc(post.Url, makePostHandler(post))
-	}
-
 	port := "8000"
 	if *deploy == true {
 		port = os.Getenv("PORT")
@@ -86,9 +46,9 @@ func main() {
 
 	srv := &http.Server{
 		Addr:         ":" + port,
-		Handler:      mux,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
+		Handler:      bl.routes(),
 	}
 
 	log.Println("Server is running on port " + port)
---
+++ routes.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/crdpa/site/blogposts"
+	"html/template"
+	"net/http"
+)
+
+func (bl *blog) httpFunc(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/", "/index.html":
+		bl.executeTemplate(w, "index.html", blogposts.FrontPage(bl.posts))
+		return
+	case "/blog":
+		bl.tag = r.URL.Query().Get("tag")
+		bl.executeTemplate(w, "blog.html", blogposts.BlogArchive(bl.posts, bl.tag))
+		return
+	default:
+		http.NotFound(w, r)
+		return
+	}
+}
+
+func (bl *blog) executeTemplate(w http.ResponseWriter, templ string, content interface{}) {
+	templates := template.Must(template.ParseGlob("./ui/html/*.html"))
+	err := templates.ExecuteTemplate(w, templ, content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (bl *blog) makePostHandler(post blogposts.Post) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bl.executeTemplate(w, "post.html", post)
+	}
+}
+
+func (bl *blog) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", bl.httpFunc)
+	mux.HandleFunc("/blog", bl.httpFunc)
+
+	for _, post := range bl.posts {
+		mux.HandleFunc(post.Url, bl.makePostHandler(post))
+	}
+
+	return mux
+}
